f2b2b72c4cb86601af7a1d1701521828e2eda192
melissamata <melissamata@outlook.com>
2017-10-17 16:44:52 -0600

    Encapsulamiento de jugadores

---
+++ src/guigato/Jugadores.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guigato;
+
+/**
+ *
+ * @author Melissa
+ */
+public class Jugadores {
+    
+    private String j1;
+    private String j2;
+    
+    public Jugadores (String j1, String j2) {
+        
+        this.j1 = j1;
+        this.j2 = j2;
+        
+    }
+
+    /**
+     * @return the j1
+     */
+    public String getJ1() {
+        return j1;
+    }
+
+    /**
+     * @return the j2
+     */
+    public String getJ2() {
+        return j2;
+    }
+    
+    
+}
